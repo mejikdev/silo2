2952124a4993007c7339712b8ddce9a44b3b9eff
chazi13 <m.chairul669@gmail.com>
2022-10-05 16:18:41 +0700

    feat: add customer page UI

---
+++ pages/customer/add.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+import { AddCustomer } from "../../features/customer";
+
+const AddCustomerPage = () => {
+  return <AddCustomer />;
+};
+
+export default AddCustomerPage;
